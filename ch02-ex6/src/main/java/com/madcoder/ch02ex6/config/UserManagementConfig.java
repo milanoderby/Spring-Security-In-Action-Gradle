0a3b3a0b8331f56441bb1f97a5aa3d9311d246a5
최용화 <yonghwa9086@naver.com>
2024-04-20 21:39:45 +0900

    Chapter2 - 실습예제6 업로드

---
+++ ch02-ex6/src/main/java/com/madcoder/ch02ex6/config/UserManagementConfig.java
@@ -0,0 +1,34 @@
+package com.madcoder.ch02ex6.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@EnableWebSecurity
+@Configuration
+public class UserManagementConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+
+        UserDetails user = User.withUsername("john")
+            .password("12345")
+            .authorities("read")
+            .build();
+
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
