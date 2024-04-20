15f9f18f99fb1c42d5ab2fec466599c539172070
최용화 <yonghwa9086@naver.com>
2024-04-20 21:34:13 +0900

    Chapter2 - 실습예제5 업로드

---
+++ ch02-ex5/src/main/java/com/madcoder/ch02ex5/config/ProjectConfig.java
@@ -0,0 +1,34 @@
+package com.madcoder.ch02ex5.config;
+
+import com.madcoder.ch02ex5.security.CustomAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class ProjectConfig {
+
+    @Autowired
+    private CustomAuthenticationProvider authenticationProvider;
+
+    @Autowired
+    public void configureGlobalAuthenticationManagerBuilder(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests((authorize) -> authorize
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
---
+++ ch02-ex5/src/test/java/com/madcoder/ch02ex5/Ch02Ex5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madcoder.ch02ex5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch02Ex5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
