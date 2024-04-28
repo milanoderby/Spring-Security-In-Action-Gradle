f5063d4d8e4de8585cda81be0e688680229feb41
최용화 <yonghwa9086@naver.com>
2024-04-28 16:47:49 +0900

    Chapter8 - 실습예제1 업로드

---
+++ ch08-ex1/src/main/java/com/madcoder/ch08ex1/config/ProjectConfig.java
@@ -0,0 +1,58 @@
+package com.madcoder.ch08ex1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        UserDetails user1 = User.withUsername("john")
+            .password("12345")
+            .roles("ADMIN")
+            .build();
+
+        UserDetails user2 = User.withUsername("jane")
+            .password("12345")
+            .roles("MANAGER")
+            .build();
+
+        manager.createUser(user1);
+        manager.createUser(user2);
+
+        return manager;
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.authorizeRequests()
+            .requestMatchers("/hello").hasRole("ADMIN")
+            .requestMatchers("/ciao").hasRole("MANAGER")
+            .anyRequest().permitAll();
+        //.anyRequest().denyAll();
+        //.anyRequest().authenticated();
+
+        return http.build();
+    }
+}
---
+++ ch08-ex1/src/test/java/com/madcoder/ch08ex1/Ch08Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madcoder.ch08ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch08Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
