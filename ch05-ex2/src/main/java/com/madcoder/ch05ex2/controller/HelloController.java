20a7338f6e714e1532e92d79aa891fd39735fc87
최용화 <yonghwa9086@naver.com>
2024-04-28 14:57:26 +0900

    Chapter5 - 실습예제2 업로드

---
+++ ch05-ex2/src/main/java/com/madcoder/ch05ex2/controller/HelloController.java
@@ -0,0 +1,71 @@
+package com.madcoder.ch05ex2.controller;
+
+import com.madcoder.ch05ex2.service.HelloService;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    private HelloService helloService;
+
+    @GetMapping("/hello")
+    public String hello(Authentication a) {
+//        SecurityContext context = SecurityContextHolder.getContext();
+//        Authentication a = context.getAuthentication();
+
+        return "Hello, " + a.getName() + "!";
+    }
+
+    @GetMapping("/bye")
+    @Async
+    public String goodbye() {
+        SecurityContext context = SecurityContextHolder.getContext();
+        String username = context.getAuthentication().getName();
+
+        return username;
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        try {
+            DelegatingSecurityContextCallable<String> contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao, " + e.submit(contextTask).get() + "!";
+        } finally {
+            e.shutdown();
+        }
+    }
+
+    @GetMapping("/hola")
+    public String hola() throws Exception {
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        e = new DelegatingSecurityContextExecutorService(e);
+        try {
+            return "Hola, " + e.submit(task).get() + "!";
+        } finally {
+            e.shutdown();
+        }
+    }
+}
---
+++ ch05-ex2/src/test/java/com/madcoder/ch05ex2/Ch05Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madcoder.ch05ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch05Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
