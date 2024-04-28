0f750e0b9f2b6ab6cad7c11ad6ad6af40a91f52f
최용화 <yonghwa9086@naver.com>
2024-04-28 14:56:03 +0900

    Chapter7 - 실습예제2 업로드

---
+++ ch07-ex2/src/test/java/com/madcoder/ch07ex2/Ch07Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madcoder.ch07ex2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch07Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
