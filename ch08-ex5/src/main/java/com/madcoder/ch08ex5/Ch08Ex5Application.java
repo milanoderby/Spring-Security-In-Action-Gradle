41714e693a22086977132c56243614d9309c6659
최용화 <yonghwa9086@naver.com>
2024-04-28 19:47:38 +0900

    Chapter8 - 실습예제5 업로드

---
+++ ch08-ex5/src/main/java/com/madcoder/ch08ex5/Ch08Ex5Application.java
@@ -0,0 +1,13 @@
+package com.madcoder.ch08ex5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch08Ex5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch08Ex5Application.class, args);
+	}
+
+}
---
+++ ch08-ex5/src/test/java/com/madcoder/ch08ex5/Ch08Ex5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madcoder.ch08ex5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch08Ex5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
