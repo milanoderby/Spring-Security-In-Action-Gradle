525923d83fd9e8dead7b3b144a00962b364c8be6
최용화 <yonghwa9086@naver.com>
2024-04-28 20:05:44 +0900

    Chapter8 - 실습예제6 업로드

---
+++ ch08-ex6/src/test/java/com/madcoder/ch08ex6/Ch08Ex6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madcoder.ch08ex6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch08Ex6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
