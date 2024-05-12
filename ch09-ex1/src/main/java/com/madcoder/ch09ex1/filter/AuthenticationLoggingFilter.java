0cb50ee7d65d25bbcc5dd73a89c26572909f89a5
최용화 <yonghwa9086@naver.com>
2024-05-12 14:55:19 +0900

    Chapter9 - 실습예제1 업로드

---
+++ ch09-ex1/src/main/java/com/madcoder/ch09ex1/filter/AuthenticationLoggingFilter.java
@@ -0,0 +1,23 @@
+package com.madcoder.ch09ex1.filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class AuthenticationLoggingFilter implements Filter {
+
+    private final Logger logger = Logger.getLogger(AuthenticationLoggingFilter.class.getName());
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestId = httpRequest.getHeader("Request-Id");
+        logger.info("Successfully authenticated request with id " +  requestId);
+        filterChain.doFilter(request, response);
+    }
+}
