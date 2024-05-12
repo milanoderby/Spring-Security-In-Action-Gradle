105113ce9f9c2d155cbb9298d7a3fb34245c3884
최용화 <yonghwa9086@naver.com>
2024-05-12 17:24:27 +0900

    Chapter10 - 실습예제2 업로드

---
+++ ch10-ex2/src/main/java/com/madcoder/ch10ex2/config/ProjectConfig.java
@@ -0,0 +1,48 @@
+package com.madcoder.ch10ex2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager uds = new InMemoryUserDetailsManager();
+
+        UserDetails u1 = User.withUsername("mary")
+            .password("12345")
+            .authorities("READ")
+            .build();
+
+        uds.createUser(u1);
+
+        return uds;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .anyRequest().authenticated();
+
+        http.formLogin()
+            .defaultSuccessUrl("/main", true);
+
+        return http.build();
+    }
+}
