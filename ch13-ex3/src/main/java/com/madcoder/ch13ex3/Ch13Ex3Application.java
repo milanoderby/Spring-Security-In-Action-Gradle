4757928c2f4e572cecd244068fc8f372db4105c4
최용화 <yonghwa9086@naver.com>
2024-05-15 22:28:31 +0900

    Chapter13 - 실습예제3 업로드

---
+++ ch13-ex3/src/main/java/com/madcoder/ch13ex3/Ch13Ex3Application.java
@@ -0,0 +1,13 @@
+package com.madcoder.ch13ex3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch13Ex3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch13Ex3Application.class, args);
+    }
+
+}
