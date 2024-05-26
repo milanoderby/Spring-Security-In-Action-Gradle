9d8e3749330e924b55d81c1ceeb58b7bf8e1e6f3
최용화 <yonghwa9086@naver.com>
2024-05-26 14:37:05 +0900

    Chapter16 - 실습예제1 업로드

---
+++ ch16-ex1/src/test/java/com/madcoder/ch16ex1/Ch16Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madcoder.ch16ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch16Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
